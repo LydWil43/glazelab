c9828ed4baa4e3d899e86a407b3334ee8e5f7e26
LydWil43 <59640275+LydWil43@users.noreply.github.com>
2026-03-29 19:55:49 -0800

    Add tags, delete test, print by tag

--- models.py
+++ models.py
@@ -17,6 +17,7 @@ class Glaze(db.Model):
     notes = db.Column(db.Text)
     lineage_notes = db.Column(db.Text)
     batch_date = db.Column(db.String(50))
+    tags = db.Column(db.String(300))  # comma-separated e.g. "liner,ash,crystalline"
     # UMF fields
     umf_expansion = db.Column(db.Float)
     umf_r2o_ro = db.Column(db.String(50))
